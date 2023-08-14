3af0fe2c4ebecf183aa346848f627581146d6298
James mensah <jamesmensahcoded@gmail.com>
2023-08-14 00:56:39 +0000

    first commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,33 +8,44 @@ const {Admin}=require("../models/users")
 
 exports.GetGeo=async()=>{
 
-  const responsefull = await fetch(
-    `https://api.ipdata.co/?api-key=${process.env.ipgeoAPI}`
-  );
-  const ipa = await responsefull.json();
-  const country = await ipa.country_name;
-  const userphonecode = await ipa.calling_code;
-  const continent = await ipa.continent_name;
-  const state_c = await ipa.region;
-  const timezone = [
-    await ipa.time_zone.name,
-    await ipa.time_zone.current_time,
-  ];
-  const ipaddress = await ipa.ip;
-  const checkSecure=(await fetch(`http://v2.api.iphub.info/ip/${ipaddress}`,{
-    headers:{"X-Key":`${process.env.IPUB}=`}
-}))
-const secure=await checkSecure.json()
- return{
-  "country":country,
-  "userphonecode" :userphonecode,
-   "timezone":timezone,
- "state_c":state_c,
-   "continent":continent,
- "ipaddress":ipaddress,
- "blockrate":secure.block
- 
- }
+  try{
+
+    const responsefull = await fetch(
+      `https://api.ipdata.co/?api-key=${process.env.ipgeoAPI}`
+    );
+    const ipa = await responsefull.json();
+    const country = await ipa.country_name;
+    const userphonecode = await ipa.calling_code;
+    const continent = await ipa.continent_name;
+    const state_c = await ipa.region;
+    const timezone = [
+      await ipa.time_zone.name,
+      await ipa.time_zone.current_time,
+    ];
+    const ipaddress = await ipa.ip;
+    const checkSecure=(await fetch(`http://v2.api.iphub.info/ip/${ipaddress}`,{
+      headers:{"X-Key":`${process.env.IPUB}=`}
+  }))
+  
+  
+  const secure=await checkSecure.json()
+   return{
+    "country":country,
+    "userphonecode" :userphonecode,
+     "timezone":timezone,
+   "state_c":state_c,
+     "continent":continent,
+   "ipaddress":ipaddress,
+   "blockrate": secure.block
+   
+   }
+
+  }
+
+  catch(error){
+
+  }
+  
 }
 
 exports.checkToken=async (req,res,next)=>{
@@ -46,7 +57,7 @@ exports.checkToken=async (req,res,next)=>{
       if(checker){
         const datas=jwt.verify(req.headers["authuser"],process.env.DB_SECRET);
        
-        console.log(datas)
+      
         const user=await User.findOne({_id:datas._id})
         if(user){
 
