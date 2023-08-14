99e07000c778aaf63dc88b40372f684eb1f7c012
James mensah <jamesmensahcoded@gmail.com>
2023-08-14 07:35:19 +0000

    first commit

--- client/src/components/Front/topcates.js
+++ client/src/components/Front/topcates.js
@@ -1,41 +1,65 @@
 import { Avatar } from "@mui/material";
 import React ,{ useEffect} from "react";
 import { CheckHover } from "../utils/reuseable";
+import { useNavigate } from "react-router-dom";
 
 const TopCate = () => {
   useEffect(()=>{
     CheckHover("topcatlayout")
   })
- 
+  const navigateTo=useNavigate();
   return (
     <div className="topcate">
       <h1>Top categories</h1>
       <div className="topcatebox">
         <div
           className="topcatlayout"
+          
+          onClick={()=>{
+
+                
+
+
+navigateTo(`/courses/category/Business`)
+                  }}
           style={{
-            backgroundImage: `url('https://s.udemycdn.com/home/top-categories/lohp-category-development-v2.jpg')`,
+            backgroundImage: `url('https://myportfoliocontent.s3.eu-north-1.amazonaws.com/ControllerImg/business.png47090.3282142807')`,
           }}
         >
-          <div className="catebtn"><p className="topcat_pi">Business man</p></div>
+          <div className="catebtn" 
+          ><p className="topcat_pi">Business</p></div>
         </div>
 
         <div
           className="topcatlayout"
+          onClick={()=>{
+
+                
+
+
+navigateTo(`/courses/category/Programming`)
+                  }}
           style={{
-            backgroundImage: `url('https://s.udemycdn.com/home/top-categories/lohp-category-development-v2.jpg')`,
+            backgroundImage: `url('https://myportfoliocontent.s3.eu-north-1.amazonaws.com/ControllerImg/programming.png52451.09205561433')`,
           }}
         >
-          <div className="catebtn"><p className="topcat_pi">Business man</p></div>
+          <div className="catebtn"><p className="topcat_pi">Programming</p></div>
         </div>
 
         <div
           className="topcatlayout"
+          onClick={()=>{
+
+                
+
+
+navigateTo(`/courses/category/Data Science`)
+                  }}
           style={{
-            backgroundImage: `url('https://s.udemycdn.com/home/top-categories/lohp-category-development-v2.jpg')`,
+            backgroundImage: `url('https://myportfoliocontent.s3.eu-north-1.amazonaws.com/ControllerImg/stack.ico30163.183381289273')`,
           }}
         >
-          <div className="catebtn"><p className="topcat_pi">Business man</p></div>
+          <div className="catebtn"><p className="topcat_pi">Data Science</p></div>
         </div>
 
 
