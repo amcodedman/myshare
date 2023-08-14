97cd6c2fe43b85b54227479cb26e1e4e4d7f1bab
James mensah <jamesmensahcoded@gmail.com>
2023-08-14 01:29:07 +0000

    first commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,18 @@
-const express=require("express")
+const express = require("express");
 require("dotenv").config();
-const jwt=require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const { User } = require("../models/users");
+const { Admin } = require("../models/users");
 
-const {User}=require("../models/users")
-const {Admin}=require("../models/users")
-
-
-exports.GetGeo=async()=>{
-
-  try{
-
-    const responsefull = await fetch(
+exports.GetGeo = async () => {
+  try {
+  
+    const responsefull = await axios.get(
       `https://api.ipdata.co/?api-key=${process.env.ipgeoAPI}`
     );
-    const ipa = await responsefull.json();
+
+    const ipa = await responsefull.data;
     const country = await ipa.country_name;
     const userphonecode = await ipa.calling_code;
     const continent = await ipa.continent_name;
@@ -23,89 +22,65 @@ exports.GetGeo=async()=>{
       await ipa.time_zone.current_time,
     ];
     const ipaddress = await ipa.ip;
-    const checkSecure=(await fetch(`http://v2.api.iphub.info/ip/${ipaddress}`,{
-      headers:{"X-Key":`${process.env.IPUB}=`}
-  }))
-  
-  
-  const secure=await checkSecure.json()
-   return{
-    "country":country,
-    "userphonecode" :userphonecode,
-     "timezone":timezone,
-   "state_c":state_c,
-     "continent":continent,
-   "ipaddress":ipaddress,
-   "blockrate": secure.block
-   
-   }
-
-  }
-
-  catch(error){
-    console.log({errorss: error})
-
-  }
-  
-}
-
-exports.checkToken=async (req,res,next)=>{
-  try {
-    
-   
-    let checker =req.headers["authuser"]
-  
-      if(checker){
-        const datas=jwt.verify(req.headers["authuser"],process.env.DB_SECRET);
-       
-      
-        const user=await User.findOne({_id:datas._id})
-        if(user){
-
-          if(user.active){
-            const responsefull = await fetch(
-              `https://api.ipdata.co/?api-key=${process.env.ipgeoAPI}`
-            );
-            const ipa = await responsefull.json();
-           
-            const userphonecode = await ipa.calling_code;
-            const continent = await ipa.continent_name;
-            const state_c = await ipa.region;
-       
-
-            res.locals.userData=user
-          }
-         
-          
-        }
-        else{
-          console.log("No user found")
-        }
-        next();
-  
+    const checkSecure = await fetch(
+      `http://v2.api.iphub.info/ip/${ipaddress}`,
+      {
+        headers: { "X-Key": `${process.env.IPUB}=` },
       }
-     
+    );
 
-
-      else{
-    
-        
-          next()
-      }
-  
-  
-      
+    const secure = await checkSecure.json();
+    return {
+      country: country,
+      userphonecode: userphonecode,
+      timezone: timezone,
+      state_c: state_c,
+      continent: continent,
+      ipaddress: ipaddress,
+      blockrate: secure.block,
+    };
   } catch (error) {
-      res.status(401).send({error:"bad token"})
-      console.log(error)
-      
+    console.log({ errorss: error });
   }
+};
+
+exports.checkToken = async (req, res, next) => {
+  try {
+    let checker = req.headers["authuser"];
+
+    if (checker) {
+      const datas = jwt.verify(req.headers["authuser"], process.env.DB_SECRET);
+
+      const user = await User.findOne({ _id: datas._id });
+      if (user) {
+        if (user.active) {
+          const responsefull = await fetch(
+            `https://api.ipdata.co/?api-key=${process.env.ipgeoAPI}`
+          );
+          const ipa = await responsefull.json();
+
+          const userphonecode = await ipa.calling_code;
+          const continent = await ipa.continent_name;
+          const state_c = await ipa.region;
+
+          res.locals.userData = user;
+        }
+      } else {
+        console.log("No user found");
+      }
+      next();
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(401).send({ error: "bad token" });
+    console.log(error);
   }
-  
-exports.Checkuser= async(req,res,next)=>{
-  const user=res.locals.userData;
- 
-  req.user=user
+};
+
+exports.Checkuser = async (req, res, next) => {
+  const user = res.locals.userData;
+
+  req.user = user;
   next();
-}
-  
+};
