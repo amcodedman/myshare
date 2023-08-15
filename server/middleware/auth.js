713b7f20ad7f72bd4461fd2427fb0a4f51745fc3
James mensah <jamesmensahcoded@gmail.com>
2023-08-15 00:20:43 +0000

    updated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -39,14 +39,7 @@ exports.checkToken = async (req, res, next) => {
       const user = await User.findOne({ _id: datas._id });
       if (user) {
         if (user.active) {
-          const responsefull = await fetch(
-            `https://api.ipdata.co/?api-key=${process.env.ipgeoAPI}`
-          );
-          const ipa = await responsefull.json();
-
-          const userphonecode = await ipa.calling_code;
-          const continent = await ipa.continent_name;
-          const state_c = await ipa.region;
+        
 
           res.locals.userData = user;
         }
